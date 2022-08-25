17565f78b42bfc7c27ae0ee6b54d6cf761781c34
davidpt <clickityclank.david@gmail.com>
2022-08-25 14:19:13 +0100

    Adicionado o boilerplate de spring com as dependências: - Spring Web - Spring Data MongoDB - Lombok

---
+++ src/main/java/pt/softinsa/projetofinal/davidnarciso/DavidNarcisoApplication.java
@@ -0,0 +1,13 @@
+package pt.softinsa.projetofinal.davidnarciso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidNarcisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidNarcisoApplication.class, args);
+	}
+
+}
