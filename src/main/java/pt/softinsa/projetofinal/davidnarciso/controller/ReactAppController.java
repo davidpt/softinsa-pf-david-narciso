a9af719ef1121582048cb1de45adce51083b6df8
davidpt <clickityclank.david@gmail.com>
2022-09-16 16:31:02 +0100

    Adicionado CRA ao  spring boot
    Adicionados dois plugins no projeto maven:
    frontend-maven-plugin - faz com que o maven instale npm e node localmente e execute o npm build na nossa pasta CRA
    maven-antrun-plugin - copia os ficheiros da pasta build da CRA para target/classes
    Adicionei um controller no spring boot com uma regex para que todos os pedidos sejam geridos pelo react à exceção dos pedidos começados por /api
    Adicionada uma linha no ficheiro application.properties - esta instrução faz com que o thymeleaf devolva os conteúdos da nossa pasta build de CRA, caso contrário iria procurar pelo index.html na pasta templates em target/classes/
    Removidos os ficheiros index e apartamentos.html, não estavam a ser usados

---
+++ src/main/java/pt/softinsa/projetofinal/davidnarciso/controller/ReactAppController.java
@@ -0,0 +1,15 @@
+package pt.softinsa.projetofinal.davidnarciso.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactAppController {
+
+	@RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}", "/error" })
+	public String getIndex(HttpServletRequest request) {
+		return "index.html";
+	}
+}
